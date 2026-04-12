d2bd3d1e60948cc97ba7ef53990051707b360b74
Ellie Pearson <ellie.jm.pearson@gmail.com>
2026-04-12 00:07:24 -0700

    prepare dat

--- robotproducer.py
+++ robotproducer.py
@@ -1,6 +1,10 @@
 import sys
 import ast
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# if using supervised learning what to do with movies with only 1 director → 
+# allowed to subsample movies and only select movies with directors with at least (4) directors 
 
 def main():
     # is input text optional?
@@ -9,36 +13,5 @@ def main():
         with open(sys.argv[1], "r") as f:
             text = f.read()
 
-def get_director(row):
-    for credit in row['crew']:
-        if credit['job'] == 'Director':
-            return credit['name']
-    return None
-
-def get_cast(row):
-    cast_mems = []
-    for mem in row['cast']:
-        cast_mems.append(mem['name'])
-    return cast_mems
-    
-
 if __name__ == "__main__":
-    credits_df = pd.read_csv("data/tmdb_5000_credits.csv")
-    movies_df = pd.read_csv("data/tmdb_5000_movies.csv")
-    df = pd.merge(movies_df, credits_df, left_on="id", right_on="movie_id")
-    df = df.drop(columns=['movie_id'])
-
-    # get director
-    df['crew'] = df['crew'].apply(ast.literal_eval)
-    df['director'] = df.apply(get_director, axis=1)
-
-
-    # get cast member names
-    df['cast'] = df['cast'].apply(ast.literal_eval)
-    df['cast_names'] = df.apply(get_cast, axis=1)
-    print(df.at[0,'cast_names'])
-
-    print(df.head())
-    print(df.columns)
-
-    # main()
+    main()
