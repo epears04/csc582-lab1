506c68dcf963a54ff7db4172b787e4cd30784ee4
Ellie Pearson <ellie.jm.pearson@gmail.com>
2026-04-13 23:55:14 -0700

    clean up

--- model.py
+++ model.py
@@ -1,8 +1,13 @@
 import ast
+import pickle
 import pandas as pd
 import numpy as np
+import scipy.sparse as sp
 from sklearn.model_selection import train_test_split
 from sentence_transformers import SentenceTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.metrics import classification_report
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
@@ -18,11 +23,17 @@ def get_attrs(row, attr):
         results.append(mem['name'])
     return results
 
-# def get_embeddings(row):
-#     overview = row['overview']
-#     if pd.isna(overview):
-#         overview = ""
-#     return model.encode(str(overview))
+def classifier(train_df, test_df, train_embeddings, test_embeddings):
+    y_train = train_df['director']
+    y_test = test_df['director']
+
+    clf = LinearSVC(max_iter=3000, class_weight='balanced', C=0.1)
+    clf.fit(train_embeddings, y_train)
+    y_pred = clf.predict(test_embeddings)
+
+    print(f"Accuracy: {(sum(y_pred == y_test)) / len(y_test):.4f}")
+    print(classification_report(y_test, y_pred, zero_division=0))
+    pickle.dump(clf, open('model/director_classifier.sav', 'wb'))
 
 if __name__ == "__main__":
     credits_df = pd.read_csv("data/tmdb_5000_credits.csv")
@@ -34,8 +45,14 @@ if __name__ == "__main__":
     # get director
     df['crew'] = df['crew'].apply(ast.literal_eval)
     df['director'] = df.apply(get_director, axis=1) 
+    df = df[df['director'] != ""]
 
-    # get cast member names
+    # drop if director appears less than 4 times
+    dir_counts = df['director'].value_counts()
+    valid_dirs = dir_counts[dir_counts >= 8].index
+    df = df[df['director'].isin(valid_dirs)]
+
+    # get cast member namesp
     df['cast'] = df['cast'].apply(ast.literal_eval)
     df['cast_names'] = df.apply(get_attrs, args=("cast",), axis=1)
 
@@ -45,7 +62,7 @@ if __name__ == "__main__":
     
     print("Size of origninal df:", df.size)
 
-    train, test = train_test_split(df, test_size=0.1, random_state=100, shuffle=True)
+    train, test = train_test_split(df, test_size=0.1, random_state=100, shuffle=True, stratify=df['director'])
 
     print("Size of train df:", train.size)
     print("Size of test df:", test.size)
@@ -53,7 +70,15 @@ if __name__ == "__main__":
     train_overviews = train['overview'].tolist()
     train_embeddings = model.encode(train_overviews)
 
+    test_overviews = test['overview'].tolist()
+    test_embeddings = model.encode(test_overviews)
+
     # save
-    np.save('data/train_embeddings.npy', train_embeddings)
+    np.save('model/train_embeddings.npy', train_embeddings)
+    np.save('model/test_embeddings.npy', test_embeddings)
     train.to_pickle('data/train_df.pkl')
     test.to_pickle('data/test_df.pkl')
+
+    train = train.reset_index(drop=True)
+    test = test.reset_index(drop=True)
+    classifier(train, test, train_embeddings, test_embeddings)
--- robotproducer.py
+++ robotproducer.py
@@ -1,17 +1,125 @@
 import sys
-import ast
-import pandas as pd
-from sklearn.model_selection import train_test_split
+import pickle 
+import random
+import numpy as np
+from sentence_transformers import SentenceTransformer
 
-# if using supervised learning what to do with movies with only 1 director → 
-# allowed to subsample movies and only select movies with directors with at least (4) directors 
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+embeddings = np.load('model/train_embeddings.npy')
+
+with open('data/train_df.pkl', 'rb') as f:
+    df = pickle.load(f)
+
+with open('model/director_classifier.sav', 'rb') as f:
+    clf = pickle.load(f)
+
+def evaluate():
+    with open('data/test_df.pkl', 'rb') as f:
+        test_df = pickle.load(f)
+
+    correct = 0
+    total_score = 0
+    for i in range(len(test_df)):
+        overview = test_df.iloc[i]['overview']
+        sim_idx, sim = get_similar(model.encode(overview))
+        embedding_predicted_dir= predict_director(sim_idx, sim)
+        ml_predicted_dir = predict_director_ml(model.encode(overview).reshape(1, -1))
+        actual_dir = test_df.iloc[i]['director']
+        actual_cast = test_df.iloc[i]['cast_names']
+        predicted_cast = predict_cast(sim_idx, sim)
+
+        score = 0
+        correct_dir = False
+
+        if embedding_predicted_dir == actual_dir:
+            score += 20
+            correct_dir = True
+            correct += 1
+            print("! from embedding")
+        if ml_predicted_dir == actual_dir:
+            if not correct_dir:
+                score += 20
+                correct += 1
+            print("! from ml")
+
+        base_hits = 0
+        bonus_hits = 0
+
+        actual_cast_set = set(actual_cast)
+        actual_top5 = set(actual_cast[:5])
+
+        for actor in predicted_cast:
+            if actor in actual_cast_set and base_hits < 5:
+                base_hits += 1
+            if actor in actual_top5:
+                bonus_hits += 1
+
+        score += 10 * min(base_hits, 5)
+        score += 5 * min(bonus_hits, 5)
+        print(f"{test_df.iloc[i]['original_title']}: {score}")
+        total_score += score
+
+    print(f"average score = {total_score / len(test_df):.2f}")
+    print(f'director accuracy = {(correct / len(test_df)) * 100:.2f}%')
+    
+def get_similar(embedding, k=100):
+    query = embedding.reshape(1, -1)
+    similarities = model.similarity(query, embeddings).flatten().numpy()
+    top_k_indices = np.argsort(similarities)[::-1][:k]
+    top_k_sims = similarities[top_k_indices]
+    return top_k_indices, top_k_sims
+
+def predict_director(top_k, top_k_sim):
+    director_scores = {}
+    for idx, score in zip(top_k, top_k_sim):
+        director = df.iloc[idx]['director']
+        s = float(score) ** 2
+        if director:
+            director_scores[director] = director_scores.get(director, 0) + s
+    
+    predicted = max(director_scores, key=director_scores.get)
+    return predicted
+
+def predict_director_ml(embedding):
+    return clf.predict(embedding)
+
+def predict_cast(top_k, top_k_sim):
+    cast_scores = {}
+    for idx, score in zip(top_k, top_k_sim):
+        cast_members = df.iloc[idx]['cast_names']
+        for mem in cast_members:
+           cast_scores[mem] = cast_scores.get(mem, 0) + float(score) ** 2
+
+    return sorted(cast_scores, key=cast_scores.get, reverse=True)[:20]
+
+def predict_movie(text):
+    embedding = model.encode(text)
+
+    top_k_idx, top_k_sim = get_similar(embedding, embeddings, k=50)
+
+    predicted_dir, director_scores = predict_director_hybrid(
+        embedding, top_k_idx, top_k_sim, clf
+    )
+
+    predicted_cast = predict_cast(
+        top_k_idx, top_k_sim, predicted_dir, top_n=10
+    )
+
+    predicted_title = suggest_title(text, top_k_idx)
+
+    return predicted_title, predicted_dir, predicted_cast
 
 def main():
-    # is input text optional?
-    if len(sys.argv) != 2: 
+    if len(sys.argv) > 2: 
         print("Usage: robotproducer.py [input.txt]")
+    elif len(sys.argv) == 1:
+        # run testing data
+        evaluate()
+    else:
+        # run normally
         with open(sys.argv[1], "r") as f:
             text = f.read()
+        predict_movie(text)
 
 if __name__ == "__main__":
     main()
